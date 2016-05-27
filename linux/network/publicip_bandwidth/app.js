43e4e6ea11cc91c0359a7addece16061b4c7b5b5
Tota77 <rafavg77@gmail.com>
2016-05-26 23:09:56 -0500

    Update app.js

--- linux/network/publicip_bandwidth/app.js
+++ linux/network/publicip_bandwidth/app.js
@@ -6,7 +6,7 @@ var date = today.getFullYear().toString() + '/'+ (today.getMonth()+1).toString()
 var speeds = speedtest();
 
 function speedtest(){
-	console.log("Launching SpeedTest, wait a moment");
+	console.log("Launching SpeedTest, wait a moment ... ");
 	test = speedTest({maxTime : 20000});
 	test.on('data',function(data){
 		requester(data);
