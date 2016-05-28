8d2e8a49fa10f86ec751481b71fab95cf62fab13
Tota77 <rafavg77@gmail.com>
2016-05-28 03:14:05 -0500

    Create me_gustas_tu.js

---
+++ javascript/stuff/me_gustas_tu.js
@@ -0,0 +1,9 @@
+var like = ["n los aviones"," viajar"," la mañana"," el viento"," soñar"," la mar"];
+
+function manu_chao(){
+	for (var i = 0; i < like.length; i++) {
+	console.log("Me gusta" + like[i] + ", me gustas tu.");
+	};
+};
+
+manu_chao();
