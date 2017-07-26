6e4c5a36604015ff1dea51c4fa5c718eb2306ff1
Tota77 <rafavg77@gmail.com>
2017-07-26 13:16:19 -0500

    Create backup_drive.sh

---
+++ linux/googledrive/backup_drive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Description:	This script run grive command to sync google drive 
+# Date:		19/17/2017
+# Author:	Rafael Vazquez
+
+# Parameters Configuration 
+DRIVE_HOME="/home/user/GoogleDrive/"
+
+# Script execution
+
+# Function to launch de google drive client and sync 
+exec_backup(){
+	cd $DRIVE_HOME
+	grive
+}
+
+# Function to verify if a grive instance is running
+isRunning(){
+	/bin/pidof grive
+	if [[ $? -ne 0 ]]; then
+		echo "grive isn't running"
+		exec_backup
+	else
+		echo "grive is running, nothing to do"
+	fi
+}
+
+isRunning
+
+# End of Script
